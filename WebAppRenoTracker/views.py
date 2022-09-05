cdbe8866c9422a290eb48adcb4577c749eb7d161
programmingwithcybineer <programmingwithcybineer@gmail.com>
2022-09-04 23:57:49 -0400

    add urls

--- WebAppRenoTracker/views.py
+++ WebAppRenoTracker/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse ("Here is your project")
+
+def projectreno(request, pk):
+    return HttpResponse ("Here is your project")
